e1632816688f985ea763e026d88b1a48f0dca403
Tom Crockett <pelotom@gmail.com>
2014-07-06 16:25:40 -0700

    Formatting

--- tests/Tests.purs
+++ tests/Tests.purs
@@ -12,19 +12,21 @@ import Test.QuickCheck
 
 ffi = unsafeForeignFunction
 
+easyConst :: Number -> Number -> Number
+easyConst = ffi ["n", ""] "n"
+
 easyAdd :: Number -> Number -> Number
 easyAdd = ffi ["x", "y"] "x + y"
 
 easySort :: [Number] -> [Number]
 easySort = ffi ["xs"] "xs.slice().sort()"
 
-easyConst :: Number -> Number -> Number
-easyConst = ffi ["n", ""] "n"
-
 main = do
   trace "Constant"
   quickCheck $ \n m -> easyConst n m == const n m
+
   trace "Addition"
   quickCheck $ \n m -> easyAdd n m == n + m
+
   trace "Sorting"
   quickCheck $ \xs -> easySort xs == sort xs
