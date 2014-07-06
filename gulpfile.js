a1f8a5a1222b3ccae2a0b01c7c40467c85b01353
Tom Crockett <pelotom@gmail.com>
2014-07-06 02:06:04 -0700

    Tidying up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,30 @@
 'use strict'
 
-var gulp      	= require('gulp')
-  , purescript 	= require('gulp-purescript')
+var gulp        = require('gulp')
+  , purescript  = require('gulp-purescript')
   ;
 
+var src = 'src/EasyFFI.purs';
+
 gulp.task('compile', function(cb) {
-	var psc = purescript.psc({
+	var psc = purescript.pscMake({
 		// Compiler options
-		output: "easy.js",
-    module: "Data.Foreign.EasyFFI"
+		output: 'output'
 	});
   psc.on('error', function(e) {
-    console.log('wtf');
     cb(e.message); // Build failed
   });
-  gulp.src(["src/*.purs"])
+  return gulp.src(src)
     .pipe(psc)
-    .pipe(gulp.dest("out"))
     .on('data', function () {
       cb(); // Completed successfully
     })
     ;
+});
+
+gulp.task('watch', function(cb) {
+  gulp.start('compile');
+  gulp.watch(src, ['compile']);
 });
 
 gulp.task('default', ['compile']);
