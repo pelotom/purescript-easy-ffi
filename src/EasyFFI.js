57031ecf7656ee1603fbbddf914bca85f92a3630
Leonardo Farroco <leonardo.farroco@gmail.com>
2022-05-05 11:40:36 -0300

    use esmodule

--- src/EasyFFI.js
+++ src/EasyFFI.js
@@ -1,11 +1,10 @@
-/* global exports */
-"use strict";
-
-exports.unsafeForeignProcedure = function(args) {
-    return function (stmt) {
-        return Function(wrap(args.slice()))();
-        function wrap() {
-            return !args.length ? stmt : 'return function (' + args.shift() + ') { ' + wrap() + ' };';
-        }
-    };
-};
+export function unsafeForeignProcedure(args) {
+  return function (stmt) {
+    return Function(wrap(args.slice()))();
+    function wrap() {
+      return !args.length
+        ? stmt
+        : 'return function (' + args.shift() + ') { ' + wrap() + ' };';
+    }
+  };
+}
